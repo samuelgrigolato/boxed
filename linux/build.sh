f18f67f36b64eb5b84b511f71f782891b78acfd5
Samuel Grigolato <samuel.grigolato@gmail.com>
2023-01-03 14:21:11 -0300

    Ensure linux build uses custom built SDL libraries

--- linux/build.sh
+++ linux/build.sh
@@ -11,6 +11,12 @@ cp -R $BASE_DIR/../common/assets $BASE_DIR/dist/assets
 gcc \
   -I$BASE_DIR/../../SDL/include \
   -I$BASE_DIR/../../SDL_image \
-  -lcurl -lSDL2 -lSDL2_image \
+  -L$BASE_DIR/../../SDL/build/.libs \
+  -L$BASE_DIR/../../SDL_image/.libs \
+  -lSDL2 -lSDL2_image \
+  -Wl,-rpath,. \
   $BASE_DIR/../common/src/main.c \
   -o $BASE_DIR/dist/boxed
+
+cp $BASE_DIR/../../SDL/build/.libs/libSDL2-2.0.so.0 $BASE_DIR/dist
+cp $BASE_DIR/../../SDL_image/.libs/libSDL2_image-2.0.so.0 $BASE_DIR/dist
